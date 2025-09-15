c0372201fcc3921d069a3be4aa229868020f2791
joao machado <jvbm2177@gmail.com>
2025-09-15 20:07:41 -0300

    notebook do coursera

--- dogs_x_cats/ex005.py
+++ dogs_x_cats/ex005.py
@@ -1,7 +1,8 @@
 import tensorflow as tf
+import pathlib
 
-train_dir = './train'
-val_dir = './test1'
+train_dir = '/home/joao/tensorflow-journey/dogs_x_cats/train'
+val_dir = pathlib.Path('/home/joao/tensorflow-journey/dogs_x_cats/test1')
 
 train_dataset = tf.keras.utils.image_dataset_from_directory(
     train_dir,
@@ -16,6 +17,9 @@ val_dataset = tf.keras.utils.image_dataset_from_directory(
     batch_size=20,
     label_mode='binary'
 )
+
+AUTOTUNE = tf.keras.AUTOTUNE
+
 
 model = tf.keras.models.Sequential(
     [
@@ -32,3 +36,5 @@ model = tf.keras.models.Sequential(
         tf.keras.layers.Dense(1, activation='sigmoid')
     ]
 )
+
+model.summary()
