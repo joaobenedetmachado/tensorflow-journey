60d343371bb964f939b6c092afeaf68e0cc0f57a
jvbm2177@gmail.com <jvbm2177@gmail.com>
2025-09-15 15:01:53 -0300

    summary and directives working

--- computer_vision_with_real_images/ex004.py
+++ computer_vision_with_real_images/ex004.py
@@ -1,14 +1,105 @@
-import tensorflow as tf
-import pandas as pd
+import os
+import random
+import numpy as np
+from io import BytesIO
 
-TRAIN_DIR = x
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+import tensorflow as tf
+
+from ipywidgets import widgets
+
+BASE_DIR = r"C:\Users\joao.193922\tensorflow-journey\computer_vision_with_real_images"
+
+train_dir = os.path.join(BASE_DIR, "horse-or-human")
+val_dir   = os.path.join(BASE_DIR, "validation-horse-or-human")
+
+train_horse_dir = os.path.join(train_dir, "horses")
+train_human_dir = os.path.join(train_dir, "humans")
+
+validation_horse_dir = os.path.join(val_dir, "horses")
+validation_human_dir = os.path.join(val_dir, "humans")
+train_horse_names = os.listdir(train_horse_dir)
+print(f'TRAIN SET HORSES: {train_horse_names[:10]}')
+
+train_human_names = os.listdir(train_human_dir)
+print(f'TRAIN SET HUMANS: {train_human_names[:10]}')
+
+validation_horse_names = os.listdir(validation_horse_dir)
+print(f'VAL SET HORSES: {validation_horse_names[:10]}')
+
+validation_human_names = os.listdir(validation_human_dir)
+print(f'VAL SET HUMANS: {validation_human_names[:10]}')
+
+nrows = 4
+ncols = 4
+
+# Set up matplotlib fig, and size it to fit 4x4 pics
+fig = plt.gcf()
+fig.set_size_inches(ncols * 3, nrows * 3)
+
+next_horse_pix = [os.path.join(train_horse_dir, fname)
+                for fname in random.sample(train_horse_names, k=8)]
+next_human_pix = [os.path.join(train_human_dir, fname)
+                for fname in random.sample(train_human_names, k=8)]
+
+for i, img_path in enumerate(next_horse_pix + next_human_pix):
+    # Set up subplot; subplot indices start at 1
+    sp = plt.subplot(nrows, ncols, i + 1)
+    sp.axis('Off') # Don't show axes (or gridlines)
+
+    img = mpimg.imread(img_path)
+    plt.imshow(img)
+
+plt.show()
+
+model = tf.keras.models.Sequential([
+    # Note the input shape is the desired size of the image 300x300 with 3 bytes color
+    # This is the first convolution
+    tf.keras.Input(shape=(300, 300, 3)),
+    tf.keras.layers.Conv2D(16, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    # The second convolution
+    tf.keras.layers.Conv2D(32, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    # The third convolution
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    # The fourth convolution
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    # The fifth convolution
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    # Flatten the results to feed into a DNN
+    tf.keras.layers.Flatten(),
+    # 512 neuron hidden layer
+    tf.keras.layers.Dense(512, activation='relu'),
+    # Only 1 output neuron. It will contain a value from 0 to 1 where 0 is for 'horses' and 1 for 'humans'
+    tf.keras.layers.Dense(1, activation='sigmoid')
+])
+
+model.summary()
+
+model.compile(loss='binary_crossentropy', # class binaria
+              optimizer=tf.keras.optimizers.RMSprop(learning_rate=0.001),
+              metrics=['accuracy'])
 
 train_dataset = tf.keras.utils.image_dataset_from_directory(
-    TRAIN_DIR,
-    imagem_size=(28, 28),
-    batch_size=128,
+    train_dir,
+    image_size=(300, 300),
+    batch_size=32,
     label_mode='binary'
-) 
+    )
+
+validation_dataset = tf.keras.utils.image_dataset_from_directory(
+    val_dir,
+    image_size=(300, 300),
+    batch_size=32,
+    label_mode='binary'
+    )
+
 
 rescale_layer = tf.keras.layers.Rescaling(scale = 1./255)
 
@@ -22,17 +113,3 @@ train_dataset_final = (
     .shuffle(buffer_size=1000)
     .prefetch(buffer_size=tf.data.AUTOTUNE)
 )
-
-model = tf.keras.models.Sequential(
-    tf.keras.Input(shape=(300, 300, 3)),
-    tf.keras.layers.Conv2D(16,(3,3), activation='relu'), # relu = valores negativos sao descartados
-    tf.keras.layers.MaxPooling2D(2,2),
-    tf.keras.layers.Conv2D(32,(3,3), activation='relu'),
-    tf.keras.layers.MaxPooling2D(2,2),
-    tf.keras.layers.Conv2D(32,(3,3), activation='relu'),
-    tf.keras.layers.MaxPooling2D(2,2),
-    tf.keras.layers.Flatten(),
-    tf.keras.layers.Dense(512, activation="relu"),
-    tf.keras.layers.Dense(1, activation="sigmoid") # classificacao binaria | homem x cavalo
-)
-
