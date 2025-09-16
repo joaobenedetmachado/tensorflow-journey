76060eb0610fd87d0da0f9807153e2d36b5c86ae
joao machado <jvbm2177@gmail.com>
2025-09-15 22:32:27 -0300

    starting with data augmentation

---
+++ image_manipulation/ex006.py
@@ -0,0 +1,40 @@
+import tensorflow as tf
+import pathlib
+
+train_dir = '/home/joao/tensorflow-journey/image_manipulation/train'
+val_dir = pathlib.Path('/home/joao/tensorflow-journey/image_manipulation/test1')
+
+train_dataset = tf.keras.utils.image_dataset_from_directory(
+    train_dir,
+    image_size=(150,150),
+    batch_size=20,
+    label_mode='binary'
+)
+
+val_dataset = tf.keras.utils.image_dataset_from_directory(
+    val_dir,
+    image_size=(150,150),
+    batch_size=20,
+    label_mode='binary'
+)
+
+AUTOTUNE = tf.keras.AUTOTUNE
+
+
+model = tf.keras.models.Sequential(
+    [
+        tf.keras.Input(shape=(150, 150, 3)),
+        tf.keras.layers.Rescaling(1./255),
+        tf.keras.layers.Conv2D(16, (3, 3), activation='relu'),  # relu = valores negativos sao descartados
+        tf.keras.layers.MaxPooling2D(2,2),
+        tf.keras.layers.Conv2D(16, (3, 3), activation='relu'),
+        tf.keras.layers.MaxPooling2D(2,2),
+        tf.keras.layers.Conv2D(16, (3, 3), activation='relu'),
+        tf.keras.layers.MaxPooling2D(2,2),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(512, activation='relu'),
+        tf.keras.layers.Dense(1, activation='sigmoid')
+    ]
+)
+
+model.summary()
