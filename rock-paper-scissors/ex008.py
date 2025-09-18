834869bfd45d1e0a9d06315c84e1ef5ddd95af86
joao machado <jvbm2177@gmail.com>
2025-09-18 19:07:58 -0300

    rock paper scrissors

--- rock-paper-scissors/ex008.py
+++ rock-paper-scissors/ex008.py
@@ -17,3 +17,35 @@ test_dataset = tf.keras.utils.image_dataset_from_directory(
     batch_size=20,
     label_mode='categorical'
 )
+
+model = tf.keras.models.Sequential([
+    tf.keras.Input(shape=(150, 150, 3)),
+    # Rescale the image. Note the input shape is the desired size of the image: 150x150 with 3 bytes for color
+    tf.keras.layers.Rescaling(1./255),
+    # This is the first convolution
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    # The second convolution
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    # The third convolution
+    tf.keras.layers.Conv2D(128, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    # The fourth convolution
+    tf.keras.layers.Conv2D(128, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    # Flatten the results to feed into a DNN
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dropout(0.5),
+    # 512 neuron hidden layer
+    tf.keras.layers.Dense(512, activation='relu'),
+    tf.keras.layers.Dense(3, activation='softmax') # 3 output neurons for 3 classes of images
+])
+
+
+
+model.compile(
+    loss='categorical_crossentropy',
+    optimizer=tf.keras.optimizers.RMSprop(learning_rate=0.001),
+    metrics=['accuracy']
+)
