a1d507f1080d7769c76ee07af91a82749543d52f
joao machado <jvbm2177@gmail.com>
2025-09-21 19:50:36 -0300

    added nb

--- nlp/ex009.py
+++ nlp/ex009.py
@@ -19,3 +19,39 @@ for index, word in enumerate(vocabulary):
     print(f"{index}: {word}")
 
 print(sequence)
+
+sentences_dataset = tf.data.Dataset.from_tensor_slices(sentences)
+
+sequences = sentences_dataset.map(vectorize_layer)
+
+print(sequences)
+
+for sentence, sequence in zip(sentences, sequences):
+    print(f"{sentence} => {sequence}")
+
+new_data = [
+    "I love AI",
+    "I love ML",
+    "You love AI",
+    "Do you think AI is fun?",
+]
+
+new_vectorize_layer = tf.keras.layers.TextVectorization()
+
+new_vectorize_layer.adapt(new_data)
+
+test_data = [
+    "i really love ai and ml",
+    "do you think ai is the future?",
+]
+
+test_sequence = new_vectorize_layer(test_data)
+
+print(test_sequence)
+
+new_vocabulary = new_vectorize_layer.get_vocabulary(include_special_tokens=False)
+
+new_sequence = new_vectorize_layer(new_data)
+
+for index, word in enumerate(new_vocabulary):
+    print(f"{index}: {word}")
