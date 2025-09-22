1b4d3f666174c4c20d1d272e5467e4825f27cc22
joao machado <jvbm2177@gmail.com>
2025-09-22 20:30:46 -0300

    padding fun

--- embedding/ex010.py
+++ embedding/ex010.py
@@ -1,4 +1,5 @@
 import tensorflow_datasets as tfds
+import tensorflow as tf
 
 imdb, info = tfds.load("imdb_reviews", with_info=True, as_supervised=True)
 
@@ -15,3 +16,22 @@ train_labels = train_data.map(lambda review, label: label)
 
 test_reviews = test_data.map(lambda review, label: review)
 test_labels = test_data.map(lambda review, label: label)
+
+vectorize_layer = tf.keras.layers.TextVectorization(max_tokens=10000)
+
+vectorize_layer.adapt(train_reviews)
+
+def padding(sequences):
+    sequences = sequences.ragged_batch(batch_size=sequences.cardinality())
+    sequences = sequences.get_single_element()
+
+    padded_seq = tf.keras.utils.pad_sequences(sequences.numpy(), maxlen=120,
+                                              truncating='post', padding='pre')
+    
+    padded_seq = tf.data.Dataset.from_tensor_slices(padded_seq)
+
+    return padded_seq
+
+
+train_sequences = train_reviews.map(lambda text: vectorize_layer(text).apply(padding))
+test_sequences = test_reviews.map(lambda text: vectorize_layer(text).apply(padding))
