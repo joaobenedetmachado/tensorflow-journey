7d1cb06b0e98126577b5286fb9881eee8c7757e9
joao machado <jvbm2177@gmail.com>
2025-09-27 09:47:54 -0300

    input sequences

--- nlp_gen/013.py
+++ nlp_gen/013.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import numpy as np
 
 data = 'In the town of Athy one Jeremy Lanigan\nBattered away til he hadn\'t a pound.\nHis father died and made him a'
 
@@ -10,3 +11,22 @@ vectorize_layer.adapt(corpus)
 
 vocabulary = vectorize_layer.get_vocabulary()
 vocab_size = len(vocabulary)
+
+input_sequences = []
+
+for line in corpus:
+    sequence = vectorize_layer(line).numpy()
+    for i in range(1, len(sequence)):
+        n_gram_sequence = sequence[:i + 1]
+        input_sequences.append(n_gram_sequence)
+
+max_sequence_len = max([len(x) for x in input_sequences])
+
+print(max_sequence_len)
+
+input_sequences = np.array(tf.keras.utils.pad_sequences(
+    input_sequences,
+    maxlen=max_sequence_len,
+    padding='pre'
+))
+
